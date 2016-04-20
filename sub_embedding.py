10d6c946daa8e7f9b08970f790f1a42b4b4faeb8
Makarovski <zhangyx233@gmail.com>
2016-04-20 15:16:17 -0400

    adding sub_embedding

--- sub_embedding.py
+++ sub_embedding.py
@@ -6,7 +6,17 @@ if __name__ == "__main__":
     f = open('./FB15k/prescription/best_valid_model.pkl')
     embeddings = pk.load(f)
     embedding, relationl, relationr = parse_embeddings(embeddings)
-    embedding = embedding.E.get_value() # numpy matrix
+    emb = embedding.E.get_value() # numpy matrix
     f.close()
 
-    print embedding
+    print "============ original embedding ============"
+    print emb
+    print "============ original shape ============"
+    print emb.shape
+
+    embedding.E.set_value(emb[1:3, :])
+    emb2 = embedding.E.get_value()
+    print "============ new embedding ============"
+    print emb2
+    print "============ new shape ============"
+    print emb2.shape
