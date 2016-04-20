2002e6fa2495f7a8f96f2420471f066f66f671a3
Makarovski <zhangyx233@gmail.com>
2016-04-20 15:47:20 -0400

    adding sub_embedding

--- sub_embedding.py
+++ sub_embedding.py
@@ -3,7 +3,20 @@ from model import *
 
 if __name__ == "__main__":
 
-    f = open('./FB15k/prescription/best_valid_model.pkl')
+    f = open('./data/FB15k_idx2entity.pkl')
+    idx2entity_pre = pk.load(f)
+    f.close()
+
+    f = open('../js_prescription/data/FB15k_entity2idx.pkl')
+    entity2idx_mer = pk.load(f)
+    f.close()
+    
+    idx_list = []
+    for i in range(len(idx2entity_pre)):
+        entity = idx2entity_pre[i]
+        idx_list += [entity2idx_mer[entity]]
+
+    f = open('../js_prescription/FB15k/js_prescription/best_valid_model.pkl')
     embeddings = pk.load(f)
     embedding, relationl, relationr = parse_embeddings(embeddings)
     emb = embedding.E.get_value() # numpy matrix
@@ -14,7 +27,7 @@ if __name__ == "__main__":
     print "============ original shape ============"
     print emb.shape
 
-    embedding.E.set_value(emb[1:3, :])
+    embedding.E.set_value(emb[idx_list[], :])
     emb2 = embedding.E.get_value()
     print "============ new embedding ============"
     print emb2
