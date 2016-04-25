1262e4a86d3a9a474cc6782560bea09ae93cf817
Yuxing Zhang <zhangyx233@gmail.com>
2016-04-25 19:10:53 -0400

    h

--- substitute.py
+++ substitute.py
@@ -4,20 +4,21 @@ import sys
 
 if __name__ == "__main__":
     in_file = sys.argv[1]
-    #out_file = sys.argv[2]
-    out_file = in_file
+    out_file = sys.argv[2]
+    #out_file = in_file
     f = open(in_file, 'r')
     lines = f.readlines()
     f.close()
     f = open(out_file, 'w')
     for i in lines:
         tokens = i.strip().split('\t')
-        if len(tokens) < 3
+        if len(tokens) < 3:
             continue
-        f.write(tokens[0])
-        f.write('\t')
-        f.write(tokens[1] + "_REL")
-        f.write('\t')
-        f.write(tokens[2])
-        f.write('\n')
+        f.write(i)
+        #f.write(tokens[0])
+        #f.write('\t')
+        #f.write(tokens[1] + "_REL")
+        #f.write('\t')
+        #f.write(tokens[2])
+        #f.write('\n')
     f.close()
