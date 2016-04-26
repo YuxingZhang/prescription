a1b461c16954d3da4472e84d52c128c684856c87
Makarovski <zhangyx233@gmail.com>
2016-04-25 21:44:51 -0400

    h

--- visualize_emb.py
+++ visualize_emb.py
@@ -1,24 +1,29 @@
-# extract the embeddings from the best_valid_model and visualize it using tsne
+''' extract the embeddings from the best_valid_model and visualize a test file by computing
+    rhs - lhs and visualize it using tsne
+    usage: python visualize_emb.py <input_test_file_name> <output_file_name>
+'''
 import cPickle as pk
 from model import *
 
-def load_embedding(filename):
-    '''load the embedding pickle and return the numpy matrix'''
-
-if __name__ == "__main__":
-    f = open('./data/FB15k_entity2idx.pkl')
-    ent2idx = pk.load(f) # a dic from entity to index
-    f.close()
-    f = open('./data/FB15k_idx2entity.pkl')
-    idx2ent = pk.load(f) # a dic from entity to index
-    f.close()
-
-    f = open('./FB15k/prescription/best_valid_model.pkl')
+def load_emb(filename):
+    '''load embedding from files, including embedding, relationl, relationr'''
+    f = open(filename)
     embeddings = pk.load(f)
-    embedding, relationl, relationr = parse_embeddings(embeddings)
-    embedding = embedding.E.get_value()
     f.close()
-    
+    embedding, relationl, relationr = parse_embeddings(embeddings)
+    embedding = np.transpose(embedding.E.get_value())
+    relationl = np.transpose(relationl.E.get_value())
+    relationr = np.transpose(relationr.E.get_value())
+    return embedding, relationl, relationr
+
+def load_map(filename):
+    '''load idx2entity or entity2idx from files'''
+    f = open(filename)
+    mapping = pk.load(f) # a dic from entity to index
+    f.close()
+    return mapping
+
+def print_emb(embedding):
     shape = embedding.shape
     f = open('embedding.txt', 'w')
     for i in range(shape[1]):
@@ -26,3 +31,32 @@ if __name__ == "__main__":
             f.write(str(embedding[j][i]) + ' ')
         f.write('\n')
     f.close()
+
+def parseline(line):
+    lhs, rel, rhs = line.split('\t')
+    lhs = lhs.split(' ')
+    rhs = rhs.split(' ')
+    rel = rel.split(' ')
+    return lhs, rel, rhs
+
+if __name__ == "__main__":
+    ent2idx = load_map('./data/FB15k_entity2idx.pkl') # a dic from entity to index
+    idx2ent = load_map('./data/FB15k_idx2entity.pkl') # a dic from idx to entity
+    embedding, relationl, relationr = load_emb('./FB15k/prescription/best_valid_model.pkl')
+
+    test_file = sys.argv[1] # test file name
+    output_file = sys.argv[2] # test file name
+    f = open(test_file, 'r')
+    data = f.readlines()
+    f.close()
+    f = open(output_file, 'w')
+    for line in data:
+        lhs, rel, rhs = parseline(line)
+        lidx = ent2idx[lhs]
+        ridx = ent2idx[rhs]
+        lemb = embedding[lidx, :]
+        remb = embedding[ridx, :]
+        dif = remb - lemb
+        for i in range(len(dif)):
+            f.write(' ' + str(dif[i]))
+        f.write('\n')
